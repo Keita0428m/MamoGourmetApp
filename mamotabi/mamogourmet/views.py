17d5d78414bae95da5cddee3070fa3bc79518ed4
Keita39 <takoyaki.nao39@gmail.com>
2020-06-21 19:49:27 +0900

    Indexページを追加。

--- mamotabi/mamogourmet/views.py
+++ mamotabi/mamogourmet/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'mamogourmet/index.html'
